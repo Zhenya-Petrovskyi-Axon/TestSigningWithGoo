489df9d088471a5586e630fce2f3dff44735dde4
Zhenya-Petrovskyi-Axon <epetrovskyi@axon.dev>
2021-05-17 15:31:37 +0300

    SignIn logic added

--- MyTestApp/MyTestApp/MainViewModule/LoginViewController.swift
+++ MyTestApp/MyTestApp/MainViewModule/LoginViewController.swift
@@ -9,12 +9,24 @@ import UIKit
 import GoogleSignIn
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    @IBOutlet weak var titleLabel: UILabel!
 
     override func viewDidLoad() {
       super.viewDidLoad()
         signingInstance()
+        setupButton()
+        titleLabel.text = "Hello User"
+        
     }
 
+    func setupButton() {
+        signInButton.layer.cornerRadius = 10
+        signInButton.layer.masksToBounds = true
+        signInButton.backgroundColor = .systemGray
+    }
+    
     func signingInstance() {
         GIDSignIn.sharedInstance()?.presentingViewController = self
 
